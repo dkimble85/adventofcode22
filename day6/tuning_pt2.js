50107829b4e44b9da13e686baa20f6217881820b
Danny Kimble <daniel.kimble1985@gmail.com>
2022-12-12 15:31:08 -0500

    Day 6 solution

---
+++ day6/tuning_pt2.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n')
+
+// let file = fs.createWriteStream('output.txt')
+
+const inputStr = input.toString()
+
+for (let x = 0; x < inputStr.length; x++) {
+  let tempStr = inputStr.slice(x, x + 14)
+  let counter = 0
+
+  // file.write('tempStr: ' + tempStr + '\n')
+
+  for (let i = 0; i < tempStr.length; i++) {
+    if (tempStr.split(tempStr.charAt(i)).length - 1 > 1) {
+      counter++
+    }
+  }
+
+  if (counter === 0) {
+    // file.write(x + 14 + '\ntrue \n')
+    console.log(x + 14)
+    break
+  }
+}
+
+// file.end()
